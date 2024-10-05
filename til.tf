3331cf275bef80956459d1283fe79f2adc97c249
tamashiro-syuta <syuta5825@gmail.com>
2024-10-05 20:51:35 +0900

    til用のiamを追加

---
+++ til.tf
@@ -0,0 +1,49 @@
+# NOTE: IAMユーザーの作成
+# NOTE: "til_github_actions_user"はterraform内で参照するためのエイリアス
+resource "aws_iam_user" "til_github_actions_user" {
+  name = "til_github-actions-user"
+}
+
+# NOTE: S3へのアップロード用ポリシーの作成
+resource "aws_iam_policy" "s3_operate_policy" {
+  name        = "TILImageBucketUploadPolicy"
+  description = "Allows to upload objects to the TIL Viewer Images bucket"
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "s3:PutObject",
+          "s3:GetObject",
+          "s3:ListBucket"
+        ]
+        Effect   = "Allow"
+        Resource = [
+          "${aws_s3_bucket.til-viewer.arn}",
+          "${aws_s3_bucket.til-viewer.arn}/*"
+        ]
+      }
+    ]
+  })
+}
+
+# NOTE: IAMユーザーにポリシーをアタッチ
+resource "aws_iam_user_policy_attachment" "til_github_actions_user_policy" {
+  user       = aws_iam_user.til_github_actions_user.name
+  policy_arn = aws_iam_policy.s3_operate_policy.arn
+}
+
+# NOTE: アクセスキーの作成
+resource "aws_iam_access_key" "github_actions_access_key" {
+  user = aws_iam_user.til_github_actions_user.name
+}
+
+# NOTE: 出力でアクセスキーを表示
+output "github_actions_access_key_id" {
+  value = aws_iam_access_key.github_actions_access_key.id
+}
+
+output "github_actions_secret_access_key" {
+  value = aws_iam_access_key.github_actions_access_key.secret
+  sensitive = true
+}
