6eb23eb4ff049ddb3674a141a967c16c2289d7d4
tamashiro-syuta <syuta5825@gmail.com>
2024-10-25 23:43:09 +0900

    🧱 リソースのterraform上の識別子を変更

--- secretsmanager.tf
+++ secretsmanager.tf
@@ -3,7 +3,7 @@ resource "aws_secretsmanager_secret" "adding_commits_lambda_secret" {
   description = "This secret stores the API key for the Lambda function"
 }
 
-resource "aws_secretsmanager_secret_version" "api_secret_version" {
+resource "aws_secretsmanager_secret_version" "adding_commits_lambda_secret_version" {
   secret_id = aws_secretsmanager_secret.adding_commits_lambda_secret.id
   secret_string = jsonencode({
     GITHUB_TOKEN = var.github_token
